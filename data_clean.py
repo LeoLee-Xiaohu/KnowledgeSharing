292d989ff4a226a7b9000d368be082521560e19a
LeoLee-Xiaohu <hackylee1314@gmail.com>
2022-06-16 09:36:50 +1000

    change from dev2

--- data_clean.py
+++ data_clean.py
@@ -1,5 +1,4 @@
 import os 
 import json 
 
-print('this is my project ')
-print('this is the change from dev2')
+print('this is for data validation')
--- data_process.py
+++ data_process.py
@@ -1,4 +1,5 @@
 import os 
-import json
-print('this is data proces ')
-print( 'this is the change for dvelopt 2')
+import json 
+
+print('this is my project ')
+print('this is the change from dev2')
